96d7d262b50ba540af826f4a8d08f6ef477201aa
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-14 20:39:42 +0530

    Updated

--- Interview Question/debounce.js
+++ Interview Question/debounce.js
@@ -26,3 +26,10 @@ debounce("Hey"); // exec
 //     timer = setTimeout(() => fn(...args), t);
 //   };
 // };
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
--- Interview Question/throttle.js
+++ Interview Question/throttle.js
@@ -13,3 +13,29 @@ function throttle(message) {
 throttle("Hello"); // exec
 throttle("World"); // cancelled
 throttle("Hey"); // cancelled
+
+// LeetCode - https://leetcode.com/problems/throttle/
+
+// const throttlee = (fn, t) => {
+//   let pending = false;
+//   let nextArgs;
+//   const wrapper = (...args) => {
+//     nextArgs = args;
+//     if (!pending) {
+//       fn(...args);
+//       pending = true;
+//       nextArgs = undefined;
+//       setTimeout(() => {
+//         pending = false;
+//         if (nextArgs) wrapper(...nextArgs);
+//       }, t);
+//     }
+//   };
+//   return wrapper;
+// };
+
+/**
+ * const throttled = throttle(console.log, 100);
+ * throttled("log"); // logged immediately.
+ * throttled("log"); // logged at t=100ms.
+ */
