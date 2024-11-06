8ed1ea257f86a10b00c326ffc1f6320de3bb1451
Mayur Kadam <mayurkadam200@gmail.com>
2024-11-06 13:21:46 +0530

    Update cacheApiCall.js

--- Interview Question/cacheApiCall.js
+++ Interview Question/cacheApiCall.js
@@ -1,41 +1,59 @@
 const cacheApiCall = (time) => {
-    const cache = {};
+  const cache = {};
 
-    return async (url, config = {}) => {
-        const key = `${url}-${JSON.stringify(config)}`;
-        const entry = cache[key];
+  return async (url, config = {}) => {
+    const key = `${url}-${JSON.stringify(config)}`;
+    const entry = cache[key];
 
-        if (!entry || Date.now() > entry.expiry) {
-            try {
-                console.log("Making new API call");
-                const response = await fetch(url, config).then((res) =>
-                    res.json()
-                );
-                cache[key] = { value: response, expiry: Date.now() + time };
-            } catch (err) {
-                console.log(err);
-            }
-        }
+    // Check if entry exists and is not expired
+    if (!entry || Date.now() > entry.expiry) {
+      try {
+        console.log("Making new API call");
+        const response = await fetch(url, config).then((res) => res.json());
 
-        return cache[key].value;
-    };
+        // Cache the response with an expiration time
+        cache[key] = { value: response, expiry: Date.now() + time };
+
+        // Set a timeout to delete the cache after it expires
+        setTimeout(() => {
+          if (Date.now() > cache[key]?.expiry) {
+            delete cache[key];
+            console.log(`Cache expired and removed for key: ${key}`);
+          }
+        }, time);
+      } catch (err) {
+        console.error("API call failed:", err);
+      }
+    }
+
+    // Return the cached value
+    return cache[key]?.value;
+  };
 };
 
+// Usage example
 const call = cacheApiCall(1500);
 call("https://jsonplaceholder.typicode.com/todos/1").then((data) =>
-    console.log(data)
+  console.log(data)
 );
 
 setTimeout(() => {
-    // Return data from cahce
-    call("https://jsonplaceholder.typicode.com/todos/1").then((data) =>
-        console.log(data)
-    );
+  // Returns data from cache if within cache time
+  call("https://jsonplaceholder.typicode.com/todos/1").then((data) =>
+    console.log(data)
+  );
 }, 1000);
 
 setTimeout(() => {
-    // Making new API call
-    call("https://jsonplaceholder.typicode.com/todos/1").then((data) =>
-        console.log(data)
-    );
-}, 2000);
+  // Makes a new API call after cache expires
+  call("https://jsonplaceholder.typicode.com/todos/1").then((data) =>
+    console.log(data)
+  );
+}, 4000);
+
+setTimeout(() => {
+  // Returns data from cache if within cache time
+  call("https://jsonplaceholder.typicode.com/todos/1").then((data) =>
+    console.log(data)
+  );
+}, 5000);
